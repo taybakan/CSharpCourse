3e7e150364898b7ca647d717dba0a8e742db890b
Taha Cemal Aybakan <taybakan@outlook.com>
2023-10-24 22:02:05 +0400

    Methods

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -20,8 +20,13 @@ namespace Methods
             int number1 = 20;
             int number2 = 100;
             var result2 = Add3(ref number1, number2);
-
             Console.WriteLine(number1);
+
+            Console.WriteLine(Multiply(2, 4));
+            Console.WriteLine(Multiply(2, 4, 5));
+
+            Console.WriteLine(Add4(2, 4, 5, 6));
+
             Console.ReadLine();
         }
 
@@ -39,6 +44,21 @@ namespace Methods
         {
             number1 += 10;
             return number1 + number2;
+        }
+
+        static int Multiply(int number1, int number2)
+        {
+            return number1 * number2;
+        }
+
+        static int Multiply(int number1, int number2, int number3)
+        {
+            return number1 * number2 * number3;
+        }
+
+        static int Add4(params int[] numbers)
+        {
+            return numbers.Sum();
         }
     }
 }
