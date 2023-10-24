09a2f122dace2f8e287d24e83d16f039e22ec5f9
Taha Cemal Aybakan <taybakan@outlook.com>
2023-10-25 01:50:37 +0400

    Final

--- Classes/Customer.cs
+++ Classes/Customer.cs
@@ -9,7 +9,12 @@ namespace Classes
     class Customer
     {
         public int Id { get; set; }
-        public string FirstName { get; set; }
+        private string _firstName;
+        public string FirstName
+        {
+            get { return "Mr. " + _firstName; }
+            set { _firstName = value; }
+        }
         public string LastName { get; set; }
         public string City { get; set; }
 
