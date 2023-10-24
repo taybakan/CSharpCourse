903b4e31e69d146de58d4817de27ae850ab9d48a
Taha Cemal Aybakan <taybakan@outlook.com>
2023-10-25 00:29:21 +0400

    Arrays

---
+++ Arrays/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] students = new string[3];
+            students[0] = "Cemal";
+            students[1] = "Mustafa";
+            students[2] = "Fatih";
+
+            string[] students2 = { "Bilal", "Tolga", "Senem" };
+            //students2[3] = "Yusuf";
+
+            foreach (var student in students2)
+            {
+                Console.WriteLine(student);
+            }
+
+            string[,] regions= new string[5,3]
+            {
+                {"İstanbul", "İzmit", "Tekirdag"},
+                {"Ankara", "Konya", "Kırıkkale"},
+                {"Antalya", "Adana", "Mersin"},
+                {"Samsun", "Ordu", "Giresun"},
+                {"İzmir", "Muğla", "Afyonkarahisar"}
+            };
+
+            for (int i = 0; i <= regions.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= regions.GetUpperBound(1); j++)
+                    Console.WriteLine(regions[i,j]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
