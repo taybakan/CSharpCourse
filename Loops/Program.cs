3c0aab34f9d92d7347587fe730dd5d2d089896cf
Taha Cemal Aybakan <taybakan@outlook.com>
2023-10-25 01:13:29 +0400

    Loops

---
+++ Loops/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ForLoop();
+            //WhileLoop();
+            //DoWhileLoop();
+            //ForEachLoop();
+            if (IsPrimeNumber(6))
+                Console.WriteLine("This is a prime number");
+            else
+                Console.WriteLine("This is not a prime number");
+            
+
+            Console.ReadLine();
+        }
+
+        private static bool IsPrimeNumber(int number)
+        {
+            bool result = true;
+
+            for (int i = 2; i < number - 1; i++)
+            {
+                if (number % i == 0)
+                {
+                    result = false;
+                    i = number;
+                }
+            }
+            return result;
+        }
+        private static void ForEachLoop()
+        {
+            string[] students = new string[3] { "Cemal", "Tolga", "Yusuf" };
+            foreach (var student in students)
+            {
+                Console.WriteLine(student);
+            }
+        }
+        private static void DoWhileLoop()
+        {
+            int number = 10;
+            do
+            {
+                Console.WriteLine(number);
+                number--;
+            } while (number >= 11);
+        }
+
+        private static void WhileLoop()
+        {
+            int number = 100;
+            while (number >= 0)
+            {
+                Console.WriteLine(number);
+                number--;
+                if (number == 52)
+                    break;
+            }
+            Console.WriteLine("Now number is {0}", number);
+        }
+        private static void ForLoop()
+        {
+            for (int i = 100; i >= 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Finished!");
+        }
+    }
+}
