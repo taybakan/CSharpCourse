828a87a7c84b11f8860e2c1786d6adce0ed17979
Taha Cemal Aybakan <taybakan@outlook.com>
2023-10-25 01:43:07 +0400

    Classes

---
+++ Classes/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            ProductManager productManager = new ProductManager();
+
+            customerManager.Add();
+            customerManager.Update();
+            productManager.Add();
+            productManager.Update();
+
+
+            Customer customer = new Customer();
+            customer.City = "Ankara";
+            customer.Id = 1;
+            customer.FirstName = "Engin";
+            customer.LastName = "Demiroğ";
+
+            Customer customer2 = new Customer
+            {
+                Id = 2,
+                City = "İstanbul",
+                FirstName = "Cemal",
+                LastName = "Aybakan"
+            };
+
+            Console.WriteLine(customer2.FirstName);
+            Console.ReadLine();
+        }
+    }
+}
