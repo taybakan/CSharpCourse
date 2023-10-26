88126efdefb36e26d109a52270747e556c65b271
Taha Cemal Aybakan <taybakan@outlook.com>
2023-10-27 00:52:31 +0300

    InterfacesDemo

---
+++ InterfacesDemo/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IWorker[] workers = new IWorker[3]
+            {
+                new Manager(),
+                new Worker(),
+                new Robot()
+            };
+
+            foreach (var worker in workers)
+            {
+                worker.Work();
+            }
+
+            IEat[] eat = new IEat[2]
+            {
+                new Manager(),
+                new Worker()
+            };
+
+            foreach (var e in eat)
+            {
+                e.Eat();
+            }
+            Console.ReadLine();
+        }
+    }
+
+    interface IWorker
+    {
+        void Work();
+    }
+
+    interface IEat
+    {
+        void Eat();
+    }
+
+    interface ISalary
+    {
+        void GetSalary();
+    }
+
+    class Manager : IWorker, ISalary, IEat
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Eating");
+        }
+
+        public void GetSalary()
+        {
+            Console.WriteLine("Getting Salary");
+        }
+
+        public void Work()
+        {
+            Console.WriteLine("Working");
+        }
+    }
+
+    class Worker : IWorker, IEat, ISalary
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Eating");
+        }
+
+        public void GetSalary()
+        {
+            Console.WriteLine("Getting Salary");
+        }
+
+        public void Work()
+        {
+            Console.WriteLine("Working");
+        }
+    }
+
+    class Robot : IWorker
+    {
+        public void Work()
+        {
+            Console.WriteLine("Working");
+        }
+    }
+}
